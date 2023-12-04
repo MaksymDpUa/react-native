2f2ceebebfe3b0aaac7a015b33f95d926cfb40b5
Maksym Ponomarenko <ponomarenko2983@gmail.com>
2023-12-04 15:59:18 +0100

    add svg"addPhoto"

--- AwesomeProject/src/components/Avatar.jsx
+++ AwesomeProject/src/components/Avatar.jsx
@@ -1,12 +1,29 @@
 import { Image, View, StyleSheet } from "react-native";
 import { AddPhotoButton } from "./AddPhotoButton";
 import AddPhoto from "../../assets/addPhoto.svg";
+import Svg, { Circle, Path, Rect } from "react-native-svg";
 export const Avatar = () => {
   return (
     <View style={styles.thumb}>
       {/* <AddPhotoButton /> */}
       {/* <AddPhoto /> */}
-      <AddPhoto></AddPhoto>
+      {/* <AddPhoto></AddPhoto> */}
+      <Svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="132"
+        height="120"
+        viewBox="0 0 132 120"
+        fill="none"
+      >
+        <Rect width="120" height="120" rx="16" fill="#F6F6F6" />
+        <Circle cx="119.5" cy="93.5" r="12" fill="white" stroke="#FF6C00" />
+        <Path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M120 87H119V93H113V94H119V100H120V94H126V93H120V87Z"
+          fill="#FF6C00"
+        />
+      </Svg>
     </View>
   );
 };
